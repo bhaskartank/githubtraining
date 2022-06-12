b6232c56b8acd9e8327f9eefef848b898c33c820
bhaskartank <30597854+bhaskartank@users.noreply.github.com>
2022-06-12 19:03:54 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,11 @@
+class student():
+  def __init__(self,rollno,name):
+    self.rollno=rollno
+    self.name=name
+  def display(self,marks):
+    print("pass ho gya")
+    self.marks=marks
+s=student(1,"monty",end=' ') 
+
+
+    
